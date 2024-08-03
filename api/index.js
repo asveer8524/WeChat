60bbbd4f3a3dbc892dae669d5c0899a73f5050ad
asveer8524 <asveer8524gmail.com>
2024-08-03 17:59:10 -0500

    Find all active connections

--- api/index.js
+++ api/index.js
@@ -4,6 +4,8 @@ const connectDB = require ("./config/dbConnection");
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const ws = require('ws'); //web sockets
+const {websocketConnection} = require('./websocketConnection/wsConnect,js');
+
 
 connectDB();
 const port=process.env.port || 5000;
@@ -30,9 +32,40 @@ const server = app.listen(port, () => {
 
 // Create a WebSocket server
 const WSS = new ws.WebSocketServer({ server });
-
+/*
 // Handle WebSocket connections when ever there is a new connection done to server ws connection will be established for eah of them
-WSS.on('connection', (connection) => {
-    console.log(`New WebSocket connection established`);
-    connection.send('Hello');
+WSS.on('connection', (connection,req) => {
+
+// We will check who all are online and send the all the online user
+// WSS.clients will give us only ids we want usernames sp we will pass a req param; from req.headers we will choose cookie (jwt token) and decript it
+
+    const cookies = req.headers.cookie;
+
+    if(cookies)
+    {
+        const tokenCookieString = cookies.split(';').find(str=>str.startsWith('token=')); // as there can be may cookies; fetch cookies for each
+        
+        if(tokenCookieString)
+        {
+            const token = tokenCookieString.split('=')[1];
+
+            if(token)
+            {
+                jwt.verify(token, process.env.JWT_SECRET, (err, userData) => {
+                    if (err) {
+                        return res.status(401).json({ error: 'User is not authorized' });
+                    }
+                    console.log(userData);
+                   // res.json(userData);
+                });
+            }
+        }
+        
+    }
+
+    });
+*/
+
+WSS.on('connection', (connection, req) => {
+    websocketConnection(connection, req);
 });
